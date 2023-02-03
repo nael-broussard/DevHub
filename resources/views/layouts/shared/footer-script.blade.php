4e8544f92d7299e1bad513906e5bace504be6dee
nael-broussard <nael.broussard@2019.icam.fr>
2023-02-03 19:06:36 +0100

    installation projet

--- resources/views/layouts/shared/footer-script.blade.php
+++
@@ -1,5 +0,0 @@
-<!-- bundle -->
-<script src="{{asset('assets/js/vendor.js')}}"></script>
-@yield('script')
-<script src="{{asset('assets/js/app.min.js')}}"></script>
-@yield('script-bottom')
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\RoutingController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -15,15 +13,6 @@ use App\Http\Controllers\RoutingController;
 |
 */
 
-Route::get('/home', function () {
-    return redirect('/index');
-})->name('home');
-
-require __DIR__ . '/auth.php';
-
-Route::group(['prefix' => '/'], function () {
-    Route::get('', [RoutingController::class, 'index'])->name('root');
-    Route::get('{first}/{second}/{third}', [RoutingController::class, 'thirdLevel'])->name('third');
-    Route::get('{first}/{second}', [RoutingController::class, 'secondLevel'])->name('second');
-    Route::get('{any}', [RoutingController::class, 'root'])->name('any');
+Route::get('/', function () {
+    return view('welcome');
 });
